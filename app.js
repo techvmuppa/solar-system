f8e2b9d69212b664cc91edb49bc83c2a56133577
Venkateswar M <132954257+techvmuppa@users.noreply.github.com>
2026-08-26 11:23:14 +0530

    modified app.js conection mongoose

--- app.js
+++ app.js
@@ -11,18 +11,13 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
 app.use(cors())
 
-mongoose.connect(process.env.MONGO_URI, {
-    user: process.env.MONGO_USERNAME,
-    pass: process.env.MONGO_PASSWORD,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}, function(err) {
-    if (err) {
-        console.log("error!! " + err)
-    } else {
-      //  console.log("MongoDB Connection Successful")
-    }
-})
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("MongoDB Connection Successful");
+    })
+    .catch((err) => {
+        console.log("error!! " + err);
+    });
 
 var Schema = mongoose.Schema;
 
