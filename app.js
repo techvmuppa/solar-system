899bc21be9b9543762b7c791b4660cc665f4692e
Venkateswar M <132954257+techvmuppa@users.noreply.github.com>
2026-08-28 23:48:58 +0530

    updated code file

--- app.js
+++ app.js
@@ -12,16 +12,17 @@ app.use(express.static(path.join(__dirname, '/')));
 app.use(cors())
 
 mongoose.connect(process.env.MONGO_URI, {
+    user: process.env.MONGO_USERNAME,
+    pass: process.env.MONGO_PASSWORD,
     useNewUrlParser: true,
     useUnifiedTopology: true
 }, function(err) {
     if (err) {
-        console.log("error!! " + err);
+        console.log("error!! " + err)
     } else {
-        console.log("MongoDB Connection Successful");
+      //  console.log("MongoDB Connection Successful")
     }
-});
-    
+})
 
 var Schema = mongoose.Schema;
 
@@ -43,7 +44,7 @@ app.post('/planet',   function(req, res) {
         id: req.body.id
     }, function(err, planetData) {
         if (err) {
-            console.error("error on read", err)
+            alert("Ooops, We only have 9 planets and a sun. Select a number from 0 - 9")
             res.send("Error in Planet Data")
         } else {
             res.send(planetData);
